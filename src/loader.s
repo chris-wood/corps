2849c410bbc6a83e99d33468bf776bb0ed5cf8d9
Christopher Wood <christopherwood07@gmail.com>
2016-02-11 14:21:58 -0800

    Correct link and loader file

--- src/loader.s
+++ src/loader.s
@@ -1,4 +1,5 @@
 global loader
+extern _sum
 
 MAGIC_NUMBER equ 0x1BADB002
 FLAGS        equ 0x0
@@ -6,19 +7,29 @@ CHECKSUM     equ -MAGIC_NUMBER
 
 KERNEL_STACK_SIZE equ 4096 ; size of the stack in 4 bytes
 
-section .text:
+section .bss
+align 4 
+kernel_stack:
+    resb KERNEL_STACK_SIZE ; reserve stack for the kernel
+
+section .text
 align 4
     dd MAGIC_NUMBER
     dd FLAGS
     dd CHECKSUM
 
-section .bss:
-align 4 
-kernel_stack:
-    resb KERNEL_STACK_SIZE ; reserve stack for the kernel
+loader:
     mov esp, kernel_stack + KERNEL_STACK_SIZE ; point esp to the start of the stack (end of memory -- it grows down in size)
 
-loader:
-    mov eax, 0xCAFEBABE
-.loop:
+    mov ebx, 0xCAFEBABE
+
+    push dword 3
+    push dword 2
+    push dword 1
+
+    call _sum
+
+    mov ecx, 0xFFFFFFFF
+
+.loop:    
     jmp .loop
